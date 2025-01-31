bb02168e6495eb50c96b4c7f21d1f885d1beac72
Reyan-Ansari <reyan0685.becse24@chitkara.edu.in>
2025-01-31 11:15:52 +0530

    Solution to Traffic Light Simulation

--- Traffic Light Simulation@@6788dd63490637cfc44e1ba9/code.c
+++ Traffic Light Simulation@@6788dd63490637cfc44e1ba9/code.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 int main() {
     char ch ;
-    scanf(" %c",&ch);
+    scanf("%c",&ch);
     switch (ch){
         case 'R':printf("Stop");
                 break;
-        case 'G':printf("GO");
+        case 'G':printf("Go");
                  break;
         case 'Y':printf("Slow Down");
                  break;
