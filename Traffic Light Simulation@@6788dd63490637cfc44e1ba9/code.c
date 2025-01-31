d1923dbdca803ca209e7d55db3a5694e87bfd6e2
Reyan-Ansari <reyan0685.becse24@chitkara.edu.in>
2025-01-31 11:07:23 +0530

    Solution to Traffic Light Simulation

---
+++ Traffic Light Simulation@@6788dd63490637cfc44e1ba9/code.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    char ch ;
+    scanf("%c",&ch);
+    switch (ch){
+        case 'R':printf("Stop");
+                break;
+        case 'G':printf("GO");
+                 break;
+        case 'Y':printf("Slow Down");
+                 break;
+        default:printf("Invalid input");
+                break; 
+    }
+    return 0;
+}
